6c6dd27b053138cd103d34465c2c87f82ed1a259
Metzger <hmetzger@kpmg.com>
2016-05-11 18:22:45 -0700

    up to the array lesson

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var formattedName = HTMLheaderName.replace("%data%", "Harry Metzger");
+var formattedRole = HTMLheaderRole.replace("%data%", "JavaScript Noob");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
